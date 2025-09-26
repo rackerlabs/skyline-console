6a301e79e752fbcba995c07ba8e29b1687bd4c26
Sowmya Nethi <sowmya.kamavaram@rackspace.com>
2025-09-26 15:54:04 +0530

    Remove unused flavorProfiles code
    Signed-off-by: Sowmya Nethi <sowmya.kamavaram@rackspace.com>

--- src/stores/octavia/flavor.js
+++ src/stores/octavia/flavor.js
@@ -1,40 +1,18 @@
-import { action } from 'mobx';
-import octaviaClient from 'client/octavia';
+import client from 'client';
 import Base from 'stores/base';
 
 export class LoadBalancerFlavorStore extends Base {
   get client() {
-    return octaviaClient.flavors;
+    return client.octavia.flavors;
   }
 
   get listWithDetail() {
     return false;
   }
 
-  @action
   async fetchList(params) {
-    // Fetch both flavors and flavorprofiles
-    const [flavors, flavorProfiles] = await Promise.all([
-      this.client.list(params),
-      octaviaClient.flavorprofiles.list(),
-    ]);
-    // Create a map for quick lookup
-    const profileMap = {};
-    flavorProfiles?.flavorprofiles.forEach((profile) => {
-      profileMap[profile.id] = profile;
-    });
-    // Merge needed fields into each flavor
-    const enabledFlavors = flavors?.flavors.filter((flavor) => flavor.enabled);
-    const merged = enabledFlavors?.map((flavor) => {
-      const profile = profileMap[flavor.flavor_profile_id] || {};
-      const flavorData = JSON.parse(profile?.flavor_data);
-      return {
-        ...flavor,
-        loadbalancer_topology: flavorData?.loadbalancer_topology,
-        compute_flavor: flavorData?.compute_flavor,
-      };
-    });
-    return merged;
+    const flavors = await this.client.list(params);
+    return flavors?.flavors?.filter((flavor) => flavor.enabled) || [];
   }
 }
 
