e6111a12dcca2ee70873dfb24705964b7812e6aa
Reet Srivastava <reet.srivastava@rackspace.com>
2026-01-23 12:54:53 +0530

    Add instance snapshot selection for rebuild

--- src/pages/compute/containers/Instance/actions/RebuildSelect.jsx
+++ src/pages/compute/containers/Instance/actions/RebuildSelect.jsx
@@ -15,6 +15,7 @@
 import { inject, observer } from 'mobx-react';
 import globalImageStore from 'stores/glance/image';
 import globalServerStore from 'stores/nova/instance';
+import globalInstanceSnapshotStore from 'stores/glance/instance-snapshot';
 import { InstanceVolumeStore } from 'stores/nova/instance-volume';
 import { SnapshotStore } from 'stores/cinder/snapshot';
 import { ModalAction } from 'containers/Action';
@@ -28,6 +29,8 @@ import {
   getImageColumns,
   canImageCreateInstance,
   getImageSystemTabs,
+  imageFormats,
+  imageStatus,
 } from 'resources/glance/image';
 import { isOsDisk } from 'resources/cinder/volume';
 
@@ -39,10 +42,16 @@ export class Rebuild extends ModalAction {
   init() {
     this.store = globalServerStore;
     this.imageStore = globalImageStore;
+    this.instanceSnapshotStore = globalInstanceSnapshotStore;
     this.instanceVolumeStore = new InstanceVolumeStore();
     this.snapshotStore = new SnapshotStore();
     this.getImages();
+    this.getInstanceSnapshots();
     this.getRootVolumeSnapshots();
+    // Initialize source state to match default value
+    this.setState({
+      source: 'image',
+    });
   }
 
   get name() {
@@ -87,6 +96,10 @@ export class Rebuild extends ModalAction {
     this.imageStore.fetchList({ all_projects: this.hasAdminRole });
   }
 
+  getInstanceSnapshots() {
+    this.instanceSnapshotStore.fetchList();
+  }
+
   async getRootVolumeSnapshots() {
     const volumes = await this.instanceVolumeStore.fetchList({
       serverId: this.item.id,
@@ -107,9 +120,45 @@ export class Rebuild extends ModalAction {
     const { name } = this.item;
     const value = {
       instance: name,
+      source: this.imageSourceType?.value || 'image',
     };
     return value;
   }
+
+  get sourceTypes() {
+    return [
+      { label: t('Image'), value: 'image' },
+      { label: t('Instance Snapshot'), value: 'instanceSnapshot' },
+    ];
+  }
+
+  get imageSourceType() {
+    return this.sourceTypes.find((it) => it.value === 'image');
+  }
+
+  get snapshotSourceType() {
+    return this.sourceTypes.find((it) => it.value === 'instanceSnapshot');
+  }
+
+  get sourceTypeIsImage() {
+    const { source } = this.state || {};
+    const sourceValue = source?.value || source;
+    return sourceValue === 'image';
+  }
+
+  get sourceTypeIsSnapshot() {
+    const { source } = this.state || {};
+    const sourceValue = source?.value || source;
+    return sourceValue === 'instanceSnapshot';
+  }
+
+  onSourceChange = (value) => {
+    // Handle wrapped value from radio button
+    const sourceValue = value?.value || value;
+    this.setState({
+      source: sourceValue,
+    });
+  };
 
   static policy = 'os_compute_api:servers:rebuild';
 
@@ -139,6 +188,48 @@ export class Rebuild extends ModalAction {
     return t('The root disk of the instance has snapshots');
   }
 
+  get snapshots() {
+    const {
+      list: { data },
+    } = this.instanceSnapshotStore;
+    return (data || []).filter((it) => it.status === 'active');
+  }
+
+  get instanceSnapshotColumns() {
+    return [
+      {
+        title: t('Name'),
+        dataIndex: 'name',
+      },
+      {
+        title: t('Disk Format'),
+        dataIndex: 'disk_format',
+        valueMap: imageFormats,
+      },
+      {
+        title: t('Min System Disk'),
+        dataIndex: 'min_disk',
+        unit: 'GiB',
+      },
+      {
+        title: t('Min Memory'),
+        dataIndex: 'min_ram',
+        render: (text) => `${text / 1024}GiB`,
+      },
+      {
+        title: t('Status'),
+        dataIndex: 'status',
+        valueMap: imageStatus,
+      },
+      {
+        title: t('Created At'),
+        dataIndex: 'created_at',
+        isHideable: true,
+        valueRender: 'sinceTime',
+      },
+    ];
+  }
+
   get formItems() {
     return [
       {
@@ -149,13 +240,29 @@ export class Rebuild extends ModalAction {
         extra: this.instanceExtra,
       },
       {
+        type: 'divider',
+      },
+      {
+        name: 'source',
+        label: t('Start Source'),
+        type: 'radio',
+        options: this.sourceTypes,
+        required: true,
+        isWrappedValue: true,
+        tip: t(
+          'The start source is a template used to rebuild an instance. You can choose an image or an instance snapshot.'
+        ),
+        onChange: this.onSourceChange,
+      },
+      {
         name: 'image',
         label: t('Operating System'),
         type: 'select-table',
         data: this.images,
         isLoading: this.imageStore.list.isLoading,
-        required: true,
+        required: this.sourceTypeIsImage,
         isMulti: false,
+        display: this.sourceTypeIsImage,
         filterParams: [
           {
             label: t('Name'),
@@ -164,9 +271,27 @@ export class Rebuild extends ModalAction {
         ],
         columns: getImageColumns(this),
         tabs: this.systemTabs,
-        defaultTabValue: this.systemTabs[0].value,
+        defaultTabValue: this.systemTabs[0]?.value,
         selectedLabel: t('Image'),
         onTabChange: this.onImageTabChange,
+      },
+      {
+        name: 'instanceSnapshot',
+        label: t('Instance Snapshot'),
+        type: 'select-table',
+        data: this.snapshots,
+        isLoading: this.instanceSnapshotStore.list.isLoading,
+        required: this.sourceTypeIsSnapshot,
+        isMulti: false,
+        hidden: !this.sourceTypeIsSnapshot,
+        display: this.sourceTypeIsSnapshot,
+        filterParams: [
+          {
+            label: t('Name'),
+            name: 'name',
+          },
+        ],
+        columns: this.instanceSnapshotColumns,
       },
     ];
   }
@@ -174,9 +299,12 @@ export class Rebuild extends ModalAction {
   onSubmit = (values) => {
     const { id } = this.item;
     const {
-      image: { selectedRowKeys = [] },
+      image: { selectedRowKeys: imageKeys = [] } = {},
+      instanceSnapshot: { selectedRowKeys: snapshotKeys = [] } = {},
     } = values;
-    return this.store.rebuild({ id, image: selectedRowKeys[0] });
+    // Instance snapshots are Glance images, so they can be used as imageRef
+    const imageId = snapshotKeys[0] || imageKeys[0];
+    return this.store.rebuild({ id, image: imageId });
   };
 }
 
