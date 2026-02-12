a88b9a87910877c2621371d280bcf6e32b168973
Sowmya Nethi <sowmya.kamavaram@rackspace.com>
2026-02-12 16:28:17 +0530

    Fix config_drive not being set when keypair login type is selected
    Signed-off-by: Sowmya Nethi <sowmya.kamavaram@rackspace.com>

--- src/pages/compute/containers/Instance/actions/StepCreate/index.jsx
+++ src/pages/compute/containers/Instance/actions/StepCreate/index.jsx
@@ -748,7 +748,7 @@ export class StepCreate extends StepAction {
         getUserData(server.adminPass, userData, username || 'root')
       );
     }
-    if (server.key_name || server.adminPass || server.user_data) {
+    if (loginType.value === 'keypair' || server.adminPass || server.user_data) {
       server.config_drive = true;
     }
     const body = {
