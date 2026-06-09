ac10da496c422bab8f1e35903d519bc616a0e9f9
Sowmya Nethi <sowmya.kamavaram@rackspace.com>
2026-06-09 17:22:58 +0530

    Fix port update to send only changed fields and avoid rename policy errors
    Signed-off-by: Sowmya Nethi <sowmya.kamavaram@rackspace.com>

--- src/pages/network/containers/Port/actions/Edit.jsx
+++ src/pages/network/containers/Port/actions/Edit.jsx
@@ -15,6 +15,7 @@
 import { inject, observer } from 'mobx-react';
 import { ModalAction } from 'containers/Action';
 import globalPortStore from 'stores/neutron/port-extension';
+import { checkPolicyRule } from 'resources/skyline/policy';
 
 const portTypes =
   'normal,macvtap,direct,baremetal,direct-physical,virtio-forwarder,smart-nic';
@@ -23,6 +24,18 @@ const portTypeItems = portTypes.split(',').map((item) => ({
   label: item,
   value: item,
 }));
+
+const manualMacOption = {
+  label: t('Manual input'),
+  value: 'manual',
+};
+
+const normalizeOptionalString = (value) => {
+  if (value === null || value === undefined) {
+    return '';
+  }
+  return String(value);
+};
 
 export class Edit extends ModalAction {
   static id = 'edit-virtual-adapter';
@@ -33,35 +46,96 @@ export class Edit extends ModalAction {
 
   static policy = 'update_port';
 
+  get canEditMacAddress() {
+    return checkPolicyRule('update_port:mac_address');
+  }
+
+  get showNotice() {
+    if (this.suppressSuccessNotice) {
+      this.suppressSuccessNotice = false;
+      return false;
+    }
+    return true;
+  }
+
   get defaultValue() {
     const { item } = this.props;
     return {
-      ...item,
-      mac_address: {
-        type: 'manual',
-        mac: item.mac_address,
-      },
+      name: item.name || '',
+      description: item.description || '',
+      ...(this.canEditMacAddress
+        ? {
+            mac_address: {
+              type: 'manual',
+              mac: item.mac_address,
+            },
+          }
+        : {}),
       'binding:vnic_type': item.binding_vnic_type,
+      more: false,
     };
   }
 
   static allowed = () => Promise.resolve(true);
 
   onSubmit = (values) => {
-    const { item: { id } = {} } = this.props;
+    const { item: { id } = {}, item } = this.props;
     const {
-      mac_address: { type, mac },
+      name,
+      description,
+      mac_address: { type, mac } = {},
+      'binding:vnic_type': vnicType,
       more,
-      ...rest
     } = values;
-    const data = {
-      ...rest,
-    };
-    if (type && type !== 'auto') {
+
+    const data = {};
+    if (normalizeOptionalString(name) !== normalizeOptionalString(item.name)) {
+      data.name = name;
+    }
+    if (
+      normalizeOptionalString(description) !==
+      normalizeOptionalString(item.description)
+    ) {
+      data.description = description;
+    }
+    if (
+      this.canEditMacAddress &&
+      type === 'manual' &&
+      mac &&
+      mac !== item.mac_address
+    ) {
       data.mac_address = mac;
+    }
+    if (more && vnicType !== item.binding_vnic_type) {
+      data['binding:vnic_type'] = vnicType;
+    }
+
+    if (Object.keys(data).length === 0) {
+      this.suppressSuccessNotice = true;
+      return Promise.resolve();
     }
     return globalPortStore.update({ id }, data);
   };
+
+  get macAddressFormItem() {
+    const { item } = this.props;
+    if (this.canEditMacAddress) {
+      return {
+        name: 'mac_address',
+        label: t('Mac Address'),
+        wrapperCol: { span: 16 },
+        type: 'mac-address',
+        required: true,
+        options: [manualMacOption],
+      };
+    }
+    return {
+      name: 'mac_address',
+      label: t('Mac Address'),
+      type: 'label',
+      content: item.mac_address || '-',
+    };
+  }
 
   get formItems() {
     const { more } = this.state;
@@ -74,22 +148,7 @@ export class Edit extends ModalAction {
         required: true,
         withoutChinese: true,
       },
-      {
-        name: 'mac_address',
-        label: t('Mac Address'),
-        wrapperCol: { span: 16 },
-        type: 'mac-address',
-        required: true,
-        // component: <MacAddressInput />,
-      },
-      {
-        name: 'binding:vnic_type',
-        label: t('Port Type'),
-        type: 'select',
-        required: true,
-        options: portTypeItems,
-        hidden: !more,
-      },
+      this.macAddressFormItem,
       {
         name: 'description',
         label: t('Description'),
@@ -99,6 +158,14 @@ export class Edit extends ModalAction {
         name: 'more',
         label: t('Advanced Options'),
         type: 'more',
+      },
+      {
+        name: 'binding:vnic_type',
+        label: t('Port Type'),
+        type: 'select',
+        required: true,
+        options: portTypeItems,
+        hidden: !more,
       },
     ];
   }
